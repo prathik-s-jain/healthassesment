ad42aab6a65788682492ceba13e7d9b372ff3cfc
Prathik S <prathik.s@happiestminds.com>
2022-11-21 13:17:00 +0530

    "initial commit"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ViewComponent } from './view/view.component';
+
+import { RouterModule,Routes } from '@angular/router';
+import { PlaceComponent } from './place/place.component';
+import { ContactComponent } from './contact/contact.component';
+const appRoutes: Routes =[    
+
+  {path:'Appointment' , component:AppComponent},
+  {path:'View', component:ViewComponent},
+  {path:'Welcome', component: WelcomeComponent},
+  {path:'Place', component: PlaceComponent},
+  {path:'Contact', component: ContactComponent},
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    ViewComponent,
+    PlaceComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [WelcomeComponent]
 })
 export class AppModule { }
